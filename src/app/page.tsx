129216a71aa84cd150fc60a7b5c74a7650e7aac3
Radded <mycooljames@hotmail.com>
2024-04-05 14:04:18 -0400

    some knobs and stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,90 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
+import Switch from "react-switch";
+import { Display } from "react-7-segment-display";
+import { Donut } from "react-dial-knob";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    // NextJS handles variables that are used as *states* differently, but these will be the core states of our program.
+    
+    // We need 9 LEDs
+    const [currentUser, setCurrentUser] = useState(0); // 0 to 8
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+    // We need two 7-segs
+    const [userTemp, setUserTemp] = useState(41);
+    const [userPressure, setUserPressure] = useState(0.69);
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+    const [actualTemp, setActualTemp] = useState(25);
+    const [actualPressure, setActualPressure] = useState(0);
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+    // We need 3 buttons
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    // We need one switch
+    const [isOn, setOn] = useState(false);
+
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div className="m-auto flex flex-col">
+
+                {/* TOP row -- controls */}
+                <div className="flex flex-row p-2 border-black border-solid border-2">
+                    <p className=" border-black border-solid border-4 w-48 p-2 m-2"><b>S.U.S.</b> Shower Unification System</p>
+                    <div className="flex flex-col m-2 mx-4 py-2">
+                        <div className={isOn ? "w-4 h-4 rounded-full bg-green-500 mx-auto" : "w-4 h-4 rounded-full bg-red-500 mx-auto"}></div>
+                        <p className="mx-auto my-auto">{isOn ? "ON" : "OFF"}</p>
+                    </div>
+                    <Switch className="mx-4 my-auto" onChange={()=>{setOn(!isOn)}} checked={isOn}/>
+                </div>
+
+                {/* Dial module -- pressure */}
+                <div className="flex flex-row p-2 border-black border-solid border-2">
+                    <div className="my-auto mx-auto flex flex-col items-center">
+                        <Donut
+                            diameter={50}
+                            min={1}
+                            max={99}
+                            step={1}
+                            value={Math.round(userPressure* 100)}
+                            onValueChange={(value)=>{setUserPressure(value / 100)}}
+                        >
+                        </Donut>
+                        <p>Pressure</p>
+                    </div>
+                    <div className="ml-auto">
+                        <Display value={Math.round(userPressure * 100)} count={2} height={80} color="red" skew/>
+                    </div>
+                </div>
+
+                {/* Dial module -- temperature */}
+                <div className="flex flex-row p-2 border-black border-solid border-2">
+                    <div className="my-auto mx-auto flex flex-col items-center">
+                        <Donut
+                            diameter={50}
+                            min={25}
+                            max={47} // Making it hotter than 47 degrees C could burn, we may want to lower this still
+                            step={1}
+                            value={Math.round(userTemp)}
+                            onValueChange={setUserTemp}
+                        >
+                        </Donut>
+                        <p>Temperature</p>
+                    </div>
+                    <div className="ml-auto">
+                        <Display value={Math.round(userTemp)} count={2} height={80} color="red" skew/>
+                    </div>
+                </div>
+
+                {/* User controls module */}
+                <div className="flex flex-col p-2 border-black border-solid border-2">
+                    {/* User select buttons */}
+                    <div>
+
+                    </div>
+                    <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-md shadow-md transition duration-300 ease-in-out transform active:translate-y-1">STORE</button>
+                </div>
+            </div>
+        </main>
+    );
 }
